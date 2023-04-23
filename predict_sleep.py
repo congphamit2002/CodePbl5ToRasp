1a20492e320c39c943b7d4e5a663f4570c4695a0
cong pham <congphamit22072002@gmail.com>
2023-04-23 17:01:17 +0700

    change path for rasp

--- predict_sleep.py
+++ predict_sleep.py
@@ -37,16 +37,10 @@ sleep_frames = 0
 #Flag canh bao 
 flag = False
 
-# khởi tạo đối số
-ap = argparse.ArgumentParser()
-ap.add_argument("-p", "--shape-predictor", required=True,
-	help="path to facial landmark predictor")
-args = vars(ap.parse_args())
-
 #khởi tạo dò tìm khuôn mặt và tải model dự đoán
 print("[INFO] loading facial landmark predictor and eye predictor...")
 detector = dlib.get_frontal_face_detector()
-predictor = dlib.shape_predictor(args["shape_predictor"])
+predictor = dlib.shape_predictor('D:/Hoc_Tap/Ki2_Nam3/PBL5/code/eye_predictor.dat')
 
 # khởi tạo luồng đọc video
 print("[INFO] loading camera...")
@@ -58,57 +52,55 @@ i_frame = 0
 while True:
 	#lấy khung , resize về chiều rộng 400pixel chuyển sanh ảnh xám
 	frame = vs.read()
-	i_frame += 1
-	if i_frame % 20 == 0:
-		frame = imutils.resize(frame, width=300)
-		gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-		# detect khuôn mặt theo ảnh xám
-		rects = detector(gray, 0)
+	frame = imutils.resize(frame, width=300)
+	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+	# detect khuôn mặt theo ảnh xám
+	rects = detector(gray, 0)
 
-	# lặp qua khuôn mặt đã detect 
-		for rect in rects:
-			#chuyển đổi bounding box của dlib sang bounding box của open cv 
-			#vẽ bounding box 
-			(x, y, w, h) = face_utils.rect_to_bb(rect)
-			cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+# lặp qua khuôn mặt đã detect 
+	for rect in rects:
+		#chuyển đổi bounding box của dlib sang bounding box của open cv 
+		#vẽ bounding box 
+		(x, y, w, h) = face_utils.rect_to_bb(rect)
+		cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
-			#sử dụng custom predict để dự đoán vị trí của mắt
-			# convert chúng sang numpy array
-			shape = predictor(gray, rect)
-			shape = face_utils.shape_to_np(shape)
-			
-			#lay toa do mat trai va mat phai
-			left_eye = shape[0:6, 0:2]
-			right_eye = shape[6:12, 0:2]
+		#sử dụng custom predict để dự đoán vị trí của mắt
+		# convert chúng sang numpy array
+		shape = predictor(gray, rect)
+		shape = face_utils.shape_to_np(shape)
+		
+		#lay toa do mat trai va mat phai
+		left_eye = shape[0:6, 0:2]
+		right_eye = shape[6:12, 0:2]
 
-			#lay eye_ratio hai mat
-			left_eye_ratio = eye_ratio(left_eye)
-			right_eye_ratio = eye_ratio(right_eye)
-			# right_eye_ratio = 0
+		#lay eye_ratio hai mat
+		left_eye_ratio = eye_ratio(left_eye)
+		right_eye_ratio = eye_ratio(right_eye)
+		# right_eye_ratio = 0
 
-			average = (left_eye_ratio + right_eye_ratio) / 2.0
+		average = (left_eye_ratio + right_eye_ratio) / 2.0
 
-			if average < eye_ratio_threshold:
-				sleep_frames += 1
+		if average < eye_ratio_threshold:
+			sleep_frames += 1
 
-				if sleep_frames >= max_sleep_frames:
-					# Ve dong chu canh bao
-					cv2.putText(frame, "WARNING!!!", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
-			else:
-				sleep_frames = 0
-				cv2.putText(frame, "EYE AVG RATIO: {:.3f}".format(average), (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 0, 0), 2)
+			if sleep_frames >= max_sleep_frames:
+				# Ve dong chu canh bao
+				cv2.putText(frame, "WARNING!!!", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+		else:
+			sleep_frames = 0
+			cv2.putText(frame, "EYE AVG RATIO: {:.3f}".format(average), (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 0, 0), 2)
 
 
-			#lặp qua các cặp tọa độ (x, y) và vẽ chúng
-			for (sX, sY) in shape:
-				cv2.circle(frame, (sX, sY), 1, (0, 0, 255), -1)
+		#lặp qua các cặp tọa độ (x, y) và vẽ chúng
+		for (sX, sY) in shape:
+			cv2.circle(frame, (sX, sY), 1, (0, 0, 255), -1)
 
-	# hiển thị frame
-		cv2.imshow("Frame", frame)
-		key = cv2.waitKey(1) & 0xFF
-		# bấm q để kết thúc
-		if key == ord("q"):
-			break
+# hiển thị frame
+	cv2.imshow("Frame", frame)
+	key = cv2.waitKey(1) & 0xFF
+	# bấm q để kết thúc
+	if key == ord("q"):
+		break
 # đóng luồng
 cv2.destroyAllWindows()
 vs.stop()
